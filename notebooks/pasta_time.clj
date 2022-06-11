261b9ebb5df7f27f2896e3e30717f3eed99b5f0b
Michael Glass <me@mike.is>
2022-06-11 14:01:09 +0200

    hide boilerplate and render a gif!

--- notebooks/pasta_time.clj
+++ notebooks/pasta_time.clj
@@ -1,11 +1,26 @@
+^{:nextjournal.clerk/visibility :hide}
 (ns pasta-time
-  (:require [nextjournal.clerk :as clerk])
-  )
+  (:require [nextjournal.clerk :as clerk]
+            [nextjournal.clerk-slideshow :as slideshow]
+            [clojure.java.io :as io])
+  (:import (java.net URL)
+           (javax.imageio ImageIO)))
 
-;; # just a title
+^{::clerk/visibility :hide
+::clerk/viewer clerk/hide-result}
+(clerk/add-viewers! [slideshow/viewer])
 
+^{::clerk/visibility :hide
+::clerk/viewer clerk/hide-result}
 (comment
-  ( clerk/serve! {} {})
+  ( clerk/serve! {})
   ( clerk/show! "notebooks/pasta_time.clj" )
   )
+
+;; # 🍝untangling spaghetti using purity and copy pasta💇‍♂️
+
+^{::clerk/visibility :hide}
+(clerk/html [:img {:src "https://media.giphy.com/media/r8rKNIA7uQi9G/giphy.gif"}])
+
+
 
